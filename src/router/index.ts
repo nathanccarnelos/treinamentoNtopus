5f8a49e1fd55f9ccc954804d2fef69cff6a46de7
nathanccarnelos <nathancoltinho@gmail.com>
2020-02-12 14:52:56 -0300

    add vuetify

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,25 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/dynamic',
+    name: 'Dynamic',
+    component: () => import('../views/Dynamic.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/functional',
+    name: 'Functional',
+    component: () => import('../views/Functional.vue'),
   },
-];
+]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+export default router
---
+++ src/views/Dynamic.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="https://picsum.photos/200/300">
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+// @ is an alias to /src
+@Component
+export default class Dynamic extends Vue {}
+</script>
